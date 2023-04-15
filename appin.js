71be243012d61f0d9d4556ab4d131061b03843d7
krishna4111 <113707093+krishna4111@users.noreply.github.com>
2023-04-15 07:32:50 +0530

    bodyParser commit

--- appin.js
+++ appin.js
@@ -2,12 +2,18 @@ const express=require('express');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware');
-    next();
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="quantity"><button type="submit">Add Product</button></form>')
 })
-app.use((req,res,next)=>{
-    console.log('into another middleware');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express</h1>');
 })
 
