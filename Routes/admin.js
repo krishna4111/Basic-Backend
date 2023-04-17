f7697422fa4ff4016e5ea8cf05028dbf4eda814d
krishna4111 <113707093+krishna4111@users.noreply.github.com>
2023-04-17 20:16:57 +0530

    basic chat app commit

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,13 +1,40 @@
 const express=require('express');
 
+const fs=require('fs');
+
 const route=express.Router();
 
-route.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="text" name="quantity"><button type="submit">Add Product</button></form>')
-})                                                              
-route.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/shop/');
+route.get('/login',(req,res,next)=>{
+
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/" method="GET"><input id="username" placeholder="Enter your name" type="text" name="title"><button type="submit">add</button></form>');
+
+})
+
+
+route.get('/',(req,res,next)=>{
+    fs.readFile('chat.txt',(err,data)=>{
+        if(err){
+            console.group(err);
+            data='No chat Exits'
+        }
+        res.send(`${data}
+    <form action="/sec" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+    <input type="text" id="message" name="message" placeholder="type a message">
+    <input type="hidden" name="username" id="username">
+    <button typr="submit">send</button>
+    </form>
+    `)
+    })
+    
+})
+
+route.post('/sec',(req,res,next)=>{
+    console.log(req.body.username);
+   console.log(req.body.message);
+   fs.writeFile( "chat.txt" , `${req.body.username}:${req.body.message}  ` , {flag : 'a'}, (err)=>{
+    err ? console.log(err) : res.redirect('/');
+   })
+
 })
 
 
--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const http=require('http');
-const route=require('./routes');
-//console.log(route.someText);
+const express=require('express');
 
-const server=http.createServer(route);
+const app=express();
 
-server.listen(3000);
+const routeAdmin=require('./Routes/admin');
+
+const bodyParser=require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+// app.use('/login',(req,res,next)=>{
+   
+//     res.send('<form action="/"  method="post"><input type="text" name="name" placeholder="Enter your name"><button type="submit">log in</button></form>')
+    
+// })
+// app.use('/',(req,res,next)=>{
+//     console.log(req.body)
+//     res.send('<form action="/"  method="post"><input type="text" name="message" placeholder="type a message"><button type="submit">send</button></form>')
+// })
+app.use(routeAdmin);
+
+app.post('/',(req,res,next)=>{
+    res.status(404).send('<h1>Error 404 This Page is Not Founded</h1>')
+})
+
+app.listen(3000);
