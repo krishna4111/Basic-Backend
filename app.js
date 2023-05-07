dc50f15d92a4318c3ce170d10ed8a9c53b570be8
krishna4111 <113707093+krishna4111@users.noreply.github.com>
2023-05-07 09:20:49 +0530

    sequelize commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
-const db=require('./util/database');
+const sequelize=require('./util/database');
 
 
 
@@ -26,4 +26,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+.sync()
+.then(result=>{
+    app.listen(3000);
+})
+.catch(err=>console.log(err));
+
--- models/product.js
+++ models/product.js
@@ -1,39 +1,27 @@
-const db=require('../util/database');
+const Sequelize=require('sequelize');
 
-module.exports = class Product {
-  constructor(id,title, imageUrl, description, price) {
-    this.id=id
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize=require('../util/database');
+
+const Product=sequelize.define('products',{
+  id:{
+    type:Sequelize.INTEGER,
+    allowNull:false,
+    autoIncrement:true,
+    primaryKey:true
+  },
+  title:Sequelize.STRING, //this syntax is used only when set the type only
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false
   }
+})
 
-
-  
-   save() {
-    return db.execute('insert into products (title,price,description,imageurl) values(?,?,?,?)',
-    [this.title,this.price,this.description,this.imageUrl]
-    );
-    
-  }
-
-static deleteById(id){
- return db.execute('delete from products where products.id=?',
-  [id]);
-}
-
-
-  static fetchAll() {
-    return db.execute('select * from products')
-  }
-
-
-  static fetchById(id){
-   
-   return db.execute('select * from products where products.id=?',
-   [id]);
-  }
-
-
-}
+module.exports=Product;
